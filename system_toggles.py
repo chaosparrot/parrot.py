7526aa55e98d525e71af9d560ce8cef9d2fad255
Kevin te Raa <kevinteraa@gmail.com>
2018-09-15 15:34:31 +0200

    FINALLY FOUND THE HOTKEY FOR TURNING ON SPEECH RECOGNITION

--- system_toggles.py
+++ system_toggles.py
@@ -15,13 +15,4 @@ def toggle_eyetracker():
 	press('f4')
 	
 def toggle_speechrec():
-	currentX, currentY = position()
-	press('f6')
-	moveTo( 830, 20, 0.2 )
-
-	# Because windows speech rec does not allow me to turn on listening with a programmatic keypress,
-	# We do it using the iris program which simulates a keypress after a while
-	sleep( 0.7 )
-	press('f6')
-		
-	moveTo( currentX, currentY )
+	hotkey('ctrl','win')
