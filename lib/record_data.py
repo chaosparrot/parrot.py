cb8c91e9bcfe05cdc48a0a2615c9effba1378d77
Kevin te Raa <kevinteraa@gmail.com>
2019-06-30 10:35:14 +0200

    Added multithreaded recording - Makes recording more than twice as fast by using a sliding window and a single consumer to handle all the data preparation

--- lib/record_data.py
+++ lib/record_data.py
@@ -13,6 +13,10 @@ from scipy.signal import blackmanharris
 from lib.machinelearning import get_loudest_freq
 import os
 import msvcrt
+from queue import *
+import threading
+import traceback
+import sys
 
 # Countdown from seconds to 0
 def countdown( seconds ):
@@ -24,7 +28,7 @@ def countdown( seconds ):
 	print("                          ", end="\r")
 	return True
 
-def record_controls():
+def record_controls( recordQueue=None ):
 	ESCAPEKEY = b'\x1b'
 	SPACEBAR = b' '
 	
@@ -32,9 +36,14 @@ def record_controls():
 		character = msvcrt.getch()
 		if( character == SPACEBAR ):
 			print( "Recording paused!" )
-			
+
 			# Pause the recording by looping until we get a new keypress
 			while( True ):
+				
+				## If the audio queue exists - make sure to clear it continuously
+				if( recordQueue != None ):
+					recordQueue.queue.clear()
+			
 				if( msvcrt.kbhit() ):
 					character = msvcrt.getch()
 					if( character == SPACEBAR ):
@@ -80,53 +89,86 @@ def record_sound():
 
 	if( countdown( 5 ) == False ):
 		return;
+		
+	non_blocking_record(threshold, frequency_threshold, WAVE_OUTPUT_FILENAME, WAVE_OUTPUT_FILE_EXTENSION)
 	
+# Consumes the recordings in a sliding window fashion - Always combining the two latest chunks together	
+def record_consumer(threshold, frequency_threshold, WAVE_OUTPUT_FILENAME, WAVE_OUTPUT_FILE_EXTENSION, audio, stream):
+	global recordQueue
+
 	files_recorded = 0
-	for j in range( 0, int(300.0 * ( 1.0 / RECORD_SECONDS ))):
-		audio = pyaudio.PyAudio()
-		 
-		# start listening
-		stream = audio.open(format=FORMAT, channels=CHANNELS,
-						rate=RATE, input=True,
-						input_device_index=INPUT_DEVICE_INDEX,
-						frames_per_buffer=CHUNK)
-		frames = []
-		intensity = []
-		frequencies = [0]
-		
-		start_time = time.time()
-		for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-			data = stream.read(CHUNK)
-			frames.append(data)
-			peak = audioop.maxpp( data, 4 ) / 32767
-			intensity.append( peak )
-			
-		highestintensity = np.amax( intensity )
-			
-		byteString = b''.join(frames)
-		fftData = np.frombuffer( byteString, dtype=np.int16 )
-		frequency = get_loudest_freq( fftData, RECORD_SECONDS )
+	j = 0
+	audioFrames = []
+	try:
+		while( True ):
+			audioFrames.append( recordQueue.get() )		
+			if( len( audioFrames ) >= 2 ):
+				j+=1
+				if( len( audioFrames ) > 2 ):
+					audioFrames = audioFrames[1:]
+					
+				intensity = [
+					audioop.maxpp( audioFrames[0], 4 ) / 32767,
+					audioop.maxpp( audioFrames[1], 4 ) / 32767
+				]
+				highestintensity = np.amax( intensity )
 				
-		fileid = "%0.2f" % ((j + 1) * RECORD_SECONDS )
-		
-		if( record_controls() == False ):
-			break;
-		
-		# stop Recording
+				byteString = b''.join(audioFrames)
+				fftData = np.frombuffer( byteString, dtype=np.int16 )
+				frequency = get_loudest_freq( fftData, RECORD_SECONDS )
+				
+				fileid = "%0.2f" % ((j) * RECORD_SECONDS )
+			
+				if( record_controls( recordQueue ) == False ):
+					stream.stop_stream()
+					break;
+					 
+				if( frequency > frequency_threshold and highestintensity > threshold ):
+					files_recorded += 1
+					print( "Files recorded: %0d - Intensity: %0d - Freq: %0d - Saving %s" % ( files_recorded, highestintensity, frequency, fileid ) )
+					waveFile = wave.open(WAVE_OUTPUT_FILENAME + fileid + WAVE_OUTPUT_FILE_EXTENSION, 'wb')
+					waveFile.setnchannels(CHANNELS)
+					waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+					waveFile.setframerate(RATE)
+					waveFile.writeframes(byteString)
+					waveFile.close()
+					waveFile.aliaaa()
+				else:
+					print( "Files recorded: %0d - Intensity: %0d - Freq: %0d" % ( files_recorded, highestintensity, frequency ) )
+	except Exception as e:
+		print( "----------- ERROR DURING RECORDING -------------- " )
+		exc_type, exc_value, exc_tb = sys.exc_info()
+		traceback.print_exception(exc_type, exc_value, exc_tb)
 		stream.stop_stream()
-		stream.close()
-		audio.terminate()
-		 
-		if( frequency > frequency_threshold and highestintensity > threshold ):
-			files_recorded += 1
-			print( "Files recorded: %0d - Intensity: %0d - Freq: %0d - Saving %s" % ( files_recorded, highestintensity, frequency, fileid ) )
-			waveFile = wave.open(WAVE_OUTPUT_FILENAME + fileid + WAVE_OUTPUT_FILE_EXTENSION, 'wb')
-			waveFile.setnchannels(CHANNELS)
-			waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-			waveFile.setframerate(RATE)
-			waveFile.writeframes(byteString)
-			waveFile.close()
-		else:
-			print( "Files recorded: %0d - Intensity: %0d - Freq: %0d" % ( files_recorded, highestintensity, frequency ) )
 
+def multithreaded_record( in_data, frame_count, time_info, status ):
+	global recordQueue
+	recordQueue.put( in_data )
+	
+	return in_data, pyaudio.paContinue
+				
+# Records a non blocking audio stream and saves the chunks onto a queue
+# The queue will be used as a sliding window over the audio, where two chunks are combined into one audio file
+def non_blocking_record(threshold, frequency_threshold, WAVE_OUTPUT_FILENAME, WAVE_OUTPUT_FILE_EXTENSION):
+	global recordQueue
+	recordQueue = Queue(maxsize=0)
 
+	audio = pyaudio.PyAudio()
+	stream = audio.open(format=FORMAT, channels=CHANNELS,
+		rate=RATE, input=True,
+		input_device_index=INPUT_DEVICE_INDEX,
+		frames_per_buffer=CHUNK,
+		stream_callback=multithreaded_record)
+		
+	consumer = threading.Thread(name='consumer', target=record_consumer, args=(threshold, frequency_threshold, WAVE_OUTPUT_FILENAME, WAVE_OUTPUT_FILE_EXTENSION, audio, stream))
+	consumer.setDaemon( True )
+	consumer.start()
+	stream.start_stream()
+
+	# wait for stream to finish (5)
+	while stream.is_active():
+		time.sleep(0.1)
+
+	stream.stop_stream()
+	stream.close()
+	audio.terminate()
