fb47365aea487612a27490632e4f6f9c0fddfd55
Kevin te Raa <kevinteraa@gmail.com>
2023-07-24 10:35:09 +0200

    Separated data loading from learn data menu and audio dataset Integrated data balancing into audio dataset

--- lib/audio_dataset.py
+++ lib/audio_dataset.py
@@ -5,14 +5,12 @@ from lib.machinelearning import *
 import numpy as np
 import random
 import math
-from lib.wav import load_wav_data_from_srt
-from lib.srt import count_total_label_ms
 
 class AudioDataset(Dataset):
 
-    def __init__(self, grouped_data_directories, settings):
-        self.paths = list( grouped_data_directories.keys() )
-        self.settings = settings
+    def __init__(self, pytorch_data):
+        self.paths = list( pytorch_data["data"].keys() )
+        print( self.paths )
         self.samples = []
         self.augmented_samples = []
         self.length = 0
@@ -21,46 +19,12 @@ class AudioDataset(Dataset):
         self.generator = torch.Generator(device=self.device)
         self.random_tensor = torch.tensor(1.0, requires_grad=False, device=self.device)
         
-        for index, label in enumerate( grouped_data_directories ):
-            directories = grouped_data_directories[ label ]
-
-            listed_files = {}
-            for directory in directories:
-                segments_directory = os.path.join(directory, "segments")
-                source_directory = os.path.join(directory, "source")                
-                if not (os.path.exists(segments_directory) and os.path.exists(source_directory)):
-                    continue
-                
-                source_files = os.listdir(source_directory)
-                srt_files = [x for x in os.listdir(segments_directory) if x.endswith(".srt")]
-                for source_file in source_files:
-                    shared_key = source_file.replace(".wav", "")
-                    
-                    possible_srt_files = [x for x in srt_files if x.startswith(shared_key)]
-                    if len(possible_srt_files) == 0:
-                        continue
-                        
-                    # Find the highest version of the segmentation for this source file
-                    srt_file = possible_srt_files[0]
-                    for possible_srt_file in possible_srt_files:
-                        current_version = int( srt_file.replace(".srt", "").replace(shared_key + ".v", "") )
-                        version = int( possible_srt_file.replace(".srt", "").replace(shared_key + ".v", "") )
-                        if version > current_version:
-                            srt_file = possible_srt_file
-                    
-                    listed_files[os.path.join(source_directory, source_file)] = os.path.join(segments_directory, srt_file)
-            listed_files_size = len( listed_files )
-
-            print( f"Loading in {label}" )
-            listed_source_files = listed_files.keys()
-            for file_index, full_filename in enumerate( listed_source_files ):
-                all_samples = load_wav_data_from_srt(listed_files[full_filename], full_filename, self.settings['FEATURE_ENGINEERING_TYPE'], False)
-                augmented_samples = load_wav_data_from_srt(listed_files[full_filename], full_filename, self.settings['FEATURE_ENGINEERING_TYPE'], False, True)
-                
-                for sample in all_samples:
-                    self.samples.append([full_filename, index, torch.tensor(sample).float()])
-                for augmented_sample in augmented_samples:
-                    self.augmented_samples.append([full_filename, index, torch.tensor(augmented_sample).float()])
+        for index, label in enumerate( pytorch_data["data"] ):
+            print( "Loading " + label + str(index) )
+            for sample in pytorch_data["data"][label]:
+                self.samples.append([sample[0], index, sample[1]])
+            for augmented_sample in pytorch_data["augmented"][label]:
+                self.augmented_samples.append([augmented_sample[0], index, augmented_sample[1]])            
 
     def set_training(self, training):
         self.training = training
--- lib/default_config.py
+++ lib/default_config.py
@@ -68,7 +68,7 @@ if( SPEECHREC_ENABLED == True ):
     SPEECHREC_ENABLED = dragonfly_spec is not None
 
 BACKGROUND_LABEL = "silence"
-AUTOMATIC_DATASET_BALANCING = False
+AUTOMATIC_DATASET_BALANCING = True
 SHOULD_FIT_INSIDE_RAM = True # Ensure the dataset fits inside RAM for faster training
 # Turning this to FALSE might crash the dataloading
 MAX_RAM = 7000000000 # 7GB of usable RAM is assumed to be the maximum size to be loaded in for data
