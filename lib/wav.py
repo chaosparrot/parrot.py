fd1634074ed6eb50fbdf3950293a281a4812b783
chaosparrot <kevinteraa@gmail.com>
2023-09-06 10:01:44 +0200

    Removed a pair of parameters from the shift augmentation to account for the latest API change

--- lib/wav.py
+++ lib/wav.py
@@ -72,7 +72,7 @@ def augment_wav_data(wavData, sample_rate):
     augmenter = Compose([
         AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.5),
         TimeStretch(min_rate=0.8, max_rate=1.25, p=0.5),
-        Shift(min_fraction=-0.5, max_fraction=0.5, p=0.5),
+        Shift(p=0.5),
     ])
     return augmenter(samples=np.array(wavData, dtype="float32"), sample_rate=sample_rate)
 
